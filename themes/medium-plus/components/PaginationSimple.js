122c2c3d27be840bed57d73cdd55f44506442036
zhuchao941 <339357762@qq.com>
2025-03-27 18:36:16 +0800

    优化PaginationSimple组件的seo表现，在页码不存在时不在页面上生成

--- themes/medium-plus/components/PaginationSimple.js
+++ themes/medium-plus/components/PaginationSimple.js
@@ -12,41 +12,44 @@ import { useGlobal } from '@/lib/global'
 const PaginationSimple = ({ page, totalPage }) => {
   const { locale } = useGlobal()
   const router = useRouter()
-  const currentPage = +page
-  const showNext = currentPage < totalPage
+  const currentPage = Math.max(1, +page) // 确保页码不小于1
+  const showNext = currentPage < totalPage && totalPage > 1 // 确保有下一页且总页数大于1
   const pagePrefix = router.asPath.split('?')[0].replace(/\/page\/[1-9]\d*/, '').replace(/\/$/, '')
+
+  // 如果当前页大于总页数，不显示分页
+  if (currentPage > totalPage) {
+    return null
+  }
 
   return (
     <div className="my-10 flex justify-between font-medium text-black dark:text-gray-100 space-x-2">
-      <Link
-        href={{
-          pathname:
-            currentPage === 2
-              ? `${pagePrefix}/`
-              : `${pagePrefix}/page/${currentPage - 1}`,
-          query: router.query.s ? { s: router.query.s } : {}
-        }}
-        passHref
-        rel="prev"
-        className={`${
-          currentPage === 1 ? 'invisible' : 'block'
-        } text-center w-full duration-200 px-4 py-2 hover:border-green-500 border-b-2 hover:font-bold`}>
-        ←{locale.PAGINATION.PREV}
-
-      </Link>
-      <Link
-        href={{
-          pathname: `${pagePrefix}/page/${currentPage + 1}`,
-          query: router.query.s ? { s: router.query.s } : {}
-        }}
-        passHref
-        rel="next"
-        className={`${
-          +showNext ? 'block' : 'invisible'
-        } text-center w-full duration-200 px-4 py-2 hover:border-green-500 border-b-2 hover:font-bold`}>
-
-        {locale.PAGINATION.NEXT}→
-      </Link>
+      {currentPage > 1 && (
+        <Link
+          href={{
+            pathname: currentPage === 2 ? `${pagePrefix}/` : `${pagePrefix}/page/${currentPage - 1}`,
+            query: router.query.s ? { s: router.query.s } : {}
+          }}
+          passHref
+          rel="prev"
+          className="block text-center w-full duration-200 px-4 py-2 hover:border-green-500 border-b-2 hover:font-bold">
+          ←{locale.PAGINATION.PREV}
+        </Link>
+      )}
+      {!currentPage > 1 && <div className="w-full" />}
+      {showNext ? (
+        <Link
+          href={{
+            pathname: `${pagePrefix}/page/${currentPage + 1}`,
+            query: router.query.s ? { s: router.query.s } : {}
+          }}
+          passHref
+          rel="next"
+          className="block text-center w-full duration-200 px-4 py-2 hover:border-green-500 border-b-2 hover:font-bold">
+          {locale.PAGINATION.NEXT}→
+        </Link>
+      ) : (
+        <div className="w-full" />
+      )}
     </div>
   )
 }
